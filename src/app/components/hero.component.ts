28f61287d898747fd8edccc4a905c434e962bb86
bradyoo3691 <tmdgurl3691@gmail.com>
2026-04-16 16:53:08 +0900

    Update hero.component.ts

--- src/app/components/hero.component.ts
+++ src/app/components/hero.component.ts
@@ -101,7 +101,7 @@ export class HeroComponent implements AfterViewInit, OnInit {
   ngOnInit() {
     this.vimeoUrl.set(
       this.sanitizer.bypassSecurityTrustResourceUrl(
-        'https://player.vimeo.com/video/1183246254?autoplay=1&loop=1&autopause=0&muted=1&background=1&controls=0'
+        'https://player.vimeo.com/video/1183580062?autoplay=1&loop=1&autopause=0&muted=1&background=1&controls=0'
       )
     );
   }
@@ -116,7 +116,7 @@ export class HeroComponent implements AfterViewInit, OnInit {
     this.showPopup.set(false);
     this.vimeoUrl.set(
       this.sanitizer.bypassSecurityTrustResourceUrl(
-        'https://player.vimeo.com/video/1183246254?autoplay=1&loop=1&autopause=0&muted=0&background=1&controls=0'
+        'https://player.vimeo.com/video/1183580062?autoplay=1&loop=1&autopause=0&muted=0&background=1&controls=0'
       )
     );
   }
